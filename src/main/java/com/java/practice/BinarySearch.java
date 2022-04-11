53c2a0fe09bb16f33ce1cf7bbe42c93a2df088b7
Deepak Meher <deepakmeher777@gmail.com>
2022-04-11 17:07:24 +0530

    Added log n time complexity algo function for Binary search

--- src/main/java/com/java/practice/BinarySearch.java
+++ src/main/java/com/java/practice/BinarySearch.java
@@ -1,16 +1,26 @@
 package com.java.practice;
 
+
+//Binary search-1 -> Find index of target in a sorted array.
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		
 		int[] nums = {-1, 3, 4, 7, 9, 23, 35, 78}; 
-		int index = search(nums, 30);
-		System.out.println(index);
+		//int index1 = search1(nums, 30);
+		//System.out.println(index1);
+		
+		int index2 = search2(nums, 78);
+		System.out.println(index2);
 	}
 	
-	//Binary search-1 -> Find index of target in a sorted array.
-	public static int search(int[] nums, int target) {
+	
+	
+	
+	//This is implemented as liner search. But not the best way to do.
+	//It has the time complexity of O(n)
+	//For better solution check the search2() implementation
+	public static int search1(int[] nums, int target) {
         int index = -1;
 		
         for(int i=0; i<nums.length; i++) {
@@ -20,5 +30,29 @@ public class BinarySearch {
         }
 		return index;
     }
+	
+	
+	//Below has the time complexity of O(log n). This is way much faster than search1() function.
+	public static int search2(int[] nums, int target) {
+        int index = -1;
+		int l = 0;
+		int r = nums.length - 1;
+		int mid = (l+r)/2;
+        
+        while (l<=r) {
+        	if (target == nums[mid]) {
+        		return mid;
+        	} else if (target > nums[mid]) {
+        		l = mid+1;
+        		mid = (l+r)/2;
+        	} else {
+        		r = mid;
+        		mid = (l+r)/2;
+        	}
+        }
+        return index;
+        
+    }
+	
 
 }
