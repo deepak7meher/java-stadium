d626472474e6117debfb117e88fa7d54471cd717
Deepak Meher <deepakmeher777@gmail.com>
2022-04-12 23:20:17 +0530

    Started implementing fibonacci series  problem

--- src/main/java/com/java/practice/FibonacciSeries.java
+++ src/main/java/com/java/practice/FibonacciSeries.java
@@ -9,6 +9,13 @@ public class FibonacciSeries {
 		for (int i=0; i<n; i++) {
 			System.out.println(fibonacci(i));
 		}
+		
+		System.out.println("\n");
+		
+		int[] series = fibonacci1(10);
+		for (int i=0; i<series.length; i++) {
+			System.out.println(series[i]);
+		}
 	}
 	
 	//Time complexity of this function is O(2^n)
@@ -25,5 +32,21 @@ public class FibonacciSeries {
 		return sum;
 	}
 	
-	
+	//bottom-top approach. 
+	public static int[] fibonacci1(int n) {
+		
+		int[] a = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			if(i==0) {
+				a[i] = 0;
+			} else if(i==1) {
+				a[i] = 1;
+			} else if(i>=2) {
+				a[i] = a[i-2] + a[i-1];
+			}
+		}
+		
+		return a;
+	}
 }
