21772ee6ed1081c34abf9d05217bc1e3d102fcb8
Deepak Meher <deepakmeher777@gmail.com>
2022-04-22 23:50:12 +0530

    Min Stack Leetcode implementation

--- src/main/java/com/leetcode/easy/MinStack.java
+++ src/main/java/com/leetcode/easy/MinStack.java
@@ -5,7 +5,12 @@ public class MinStack {
 
 	public static void main(String[] args) {
 
+		MinStack minStack = new MinStack();
 		
+		minStack.push(3);
+		minStack.pop();
+		minStack.top();
+		minStack.getMin();
 	}
 
 	
