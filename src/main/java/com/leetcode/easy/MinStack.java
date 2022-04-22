1d00735b44152ea4d5b112c8e4b174b952c51d34
Deepak Meher <deepakmeher777@gmail.com>
2022-04-22 23:48:09 +0530

    Min Stack Leetcode implementation

---
+++ src/main/java/com/leetcode/easy/MinStack.java
@@ -0,0 +1,34 @@
+package com.leetcode.easy;
+
+//https://leetcode.com/problems/min-stack/
+public class MinStack {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+	
+	public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+    	
+    	return 0;
+        
+    }
+    
+    public int getMin() {
+    	
+    	return 0;
+    }
+}
