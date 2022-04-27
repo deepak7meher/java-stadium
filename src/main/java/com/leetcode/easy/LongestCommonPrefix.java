90d28558041f6728619f46cbfa9f6a1f4ef5b61b
Deepak Meher <deepakmeher777@gmail.com>
2022-04-27 23:39:54 +0530

    Longest Common Prefix Problem - Leetcode

--- src/main/java/com/leetcode/easy/LongestCommonPrefix.java
+++ src/main/java/com/leetcode/easy/LongestCommonPrefix.java
@@ -1,5 +1,8 @@
 package com.leetcode.easy;
 
+//https://leetcode.com/problems/longest-common-prefix/
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
