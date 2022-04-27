99d8a94a96f57fdbfaa6602a5c68bc085a5014ad
Deepak Meher <deepakmeher777@gmail.com>
2022-04-27 23:37:52 +0530

    Longest Common Prefix Problem - Leetcode

---
+++ src/main/java/com/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		
+		String[] strs = {"flower","flow","flight"};
+
+		String result = longestCommonPrefix(strs);
+		System.out.println(result);
+		
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+		
+		String result = null;
+		
+		
+		return result;
+        
+    }
+}
