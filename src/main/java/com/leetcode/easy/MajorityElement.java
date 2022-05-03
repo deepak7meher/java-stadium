46779b8ee43971fbdbbc44332f297e381dd3a709
Deepak Meher <deepakmeher777@gmail.com>
2022-05-03 23:55:05 +0530

    Leetcode Majority Element Problem

--- src/main/java/com/leetcode/easy/MajorityElement.java
+++ src/main/java/com/leetcode/easy/MajorityElement.java
@@ -1,18 +1,20 @@
 package com.leetcode.easy;
 
+import java.util.Arrays;
+
 public class MajorityElement {
 
 	public static void main(String[] args) {
 		
 		int[] nums = {2,2,1,1,1,2,2};
 		int l = majorityElement(nums);
+		System.out.println(l);
 	}
 	
 	public static int majorityElement(int[] nums) {
         
-		int l = 0;
-		
-		return l;
+		Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 
 }
