2105ea0105b7c2904a402db600c4b2ad43159f1f
Deepak Meher <deepakmeher777@gmail.com>
2022-05-04 23:12:48 +0530

    Letcode ExcelSheet Column Number problem

---
+++ src/main/java/com/leetcode/easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+public class ExcelSheetColumnNumber {
+
+	public static void main(String[] args) {
+
+		int colNum = titleToNumber("AB");
+		System.out.println(colNum);
+	}
+
+	public static int titleToNumber(String columnTitle) { 
+        int l = columnTitle.length();
+        int result = 0;
+
+        for(int i=0; i<l; i++) {
+            int tempValue = (columnTitle.charAt(l-i-1) - 'A') +1;
+            System.out.println(tempValue);
+            result = result + (int)Math.pow(26,i)*tempValue;
+        }
+        return result;
+    }
+}
